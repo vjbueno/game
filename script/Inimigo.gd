e6d955880d0c95894e7f543682238762872f8e1a
Bueno Vital <buenovj@hotmail.com>
2021-11-25 21:48:48 -0300

    configurando icone de vida do player

--- script/Inimigo.gd
+++ script/Inimigo.gd
@@ -4,3 +4,13 @@ func _on_DanoPlayer_body_entered(body):
 	
 	if(body.name == "Player"):
 		print(body.name)
+
+
+func _on_DanoInimigo1_body_entered(body):
+	if(body.name == "Player"):
+		print(body.name)
+		$AnimatedSprite.animation = "morrendo"
+		
+func _on_AnimatedSprite_animation_finished():
+	if ($AnimatedSprite.animation == "morrendo"):
+		queue_free()
